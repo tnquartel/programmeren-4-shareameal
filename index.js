a33085a34b090fda2c91e6e9264c0b7f20c1a82b
Robin Schellius <r.schellius@avans.nl>
2022-04-20 15:12:47 +0200

    express toegevoegd, met basic routing

--- index.js
+++ index.js
@@ -1,14 +1,73 @@
-const http = require("http");
+const express = require("express");
+const app = express();
+const port = 3000;
 
-// const hostname = "127.0.0.1";
-const port = process.env.PORT || 3000;
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/html");
-  res.end("Hello World\n");
+let database = [];
+let id = 0;
+
+app.all("*", (req, res, next) => {
+  const method = req.method;
+  console.log(`Method ${method} is aangeroepen`);
+  next();
 });
 
-server.listen(port, () => {
-  console.log(`Server running at port ${port}`);
+app.get("/", (req, res) => {
+  res.status(200).json({
+    status: 200,
+    result: "Hello World",
+  });
+});
+
+app.post("/api/movie", (req, res) => {
+  let movie = req.body;
+  id++;
+  movie = {
+    id,
+    ...movie,
+  };
+  console.log(movie);
+  database.push(movie);
+  res.status(201).json({
+    status: 201,
+    result: database,
+  });
+});
+
+app.get("/api/movie/:movieId", (req, res, next) => {
+  const movieId = req.params.movieId;
+  console.log(`Movie met ID ${movieId} gezocht`);
+  let movie = database.filter((item) => item.id == movieId);
+  if (movie.length > 0) {
+    console.log(movie);
+    res.status(200).json({
+      status: 200,
+      result: movie,
+    });
+  } else {
+    res.status(401).json({
+      status: 401,
+      result: `Movie with ID ${movieId} not found`,
+    });
+  }
+});
+
+app.get("/api/movie", (req, res, next) => {
+  res.status(200).json({
+    status: 200,
+    result: database,
+  });
+});
+
+app.all("*", (req, res) => {
+  res.status(401).json({
+    status: 401,
+    result: "End-point not found",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
