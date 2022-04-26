4f86e15e260b753a2274867ccec3c5e92a9ee6d5
tnquartel <thomasquartel@outlook.com>
2022-04-26 10:38:56 +0200

    controllers added

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,123 @@
+let database = [];
+let id = 0;
+
+let controller = {
+  addUser: (req, res) => {
+    const result = database.filter(
+      (user) => user.emailaddress == req.body.emailaddress
+    );
+    if (result.length > 0) {
+      res.status(409).json({
+        message:
+          "User was not added to database, because the email adress is already in use",
+      });
+    } else {
+      let user = req.body;
+      id++;
+      user = {
+        id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        street: user.street,
+        city: user.city,
+        emailAdress: user.emailAdress,
+        phonenNumber: user.phoneNumber,
+        password: user.password,
+        roles: user.roles,
+      };
+      console.log(user);
+      database.push(user);
+      res.status(201).json({
+        status: 201,
+        result: database,
+      });
+    }
+  },
+  getAllUsers: (req, res) => {
+    res.status(200).json({
+      status: 200,
+      result: database,
+    });
+  },
+  getUserById: (req, res) => {
+    const userId = req.params.userId;
+    console.log(`User met ID ${userId} gezocht`);
+    let user = database.filter((item) => item.id == userId);
+    if (user.length > 0) {
+      console.log(user);
+      res.status(200).json({
+        status: 200,
+        result: user,
+      });
+    } else {
+      res.status(401).json({
+        status: 401,
+        result: `User with ID ${userId} not found`,
+      });
+    }
+  },
+  getUserProfile: (req, res) => {
+    const userId = req.params.userId;
+    console.log(`User met ID ${userId} gezocht`);
+    let user = database.filter((item) => item.id == userId);
+    if (user.length > 0) {
+      console.log(user);
+      res.status(401).json({
+        status: 401,
+        result: `Feature not realised yet`,
+      });
+    } else {
+      res.status(401).json({
+        status: 401,
+        result: `User with ID ${userId} not found, and this feature is not yet realised`,
+      });
+    }
+  },
+  updateUser: (req, res) => {
+    const userId = req.params.userId;
+    console.log(`User met ID ${userId} gezocht`);
+    const result = database.findIndex((user) => user.id == userId);
+    if (result > -1) {
+      let user = req.body;
+      database[result] = {
+        id: userId,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        street: user.street,
+        city: user.city,
+        emailAdress: user.emailAdress,
+        phoneNumber: user.phoneNumber,
+        password: user.password,
+        roles: user.roles,
+      };
+      res.status(201).json({
+        status: 201,
+        result: database[result],
+      });
+    } else {
+      res.status(401).json({
+        status: 401,
+        result: `User with ID ${userId} not found`,
+      });
+    }
+  },
+  deleteUser: (req, res) => {
+    const userId = req.params.userId;
+    console.log(`User met ID ${userId} gezocht`);
+    let user = database.filter((item) => item.id == userId);
+    if (user.length > 0) {
+      console.log(user);
+      database.splice(userId - 1, 1);
+      res.status(200).json({
+        status: 200,
+        result: `User with ID ${userId} succesfully deleted`,
+      });
+    } else {
+      res.status(401).json({
+        status: 401,
+        result: `User with ID ${userId} not found`,
+      });
+    }
+  },
+};
+module.exports = controller;
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const router = express.Router();
-
-let database = [];
-let id = 0;
+const userController = require("../controllers/user.controller");
 
 router.get("/", (req, res) => {
   res.status(200).json({
@@ -11,127 +9,16 @@ router.get("/", (req, res) => {
   });
 });
 
-router.post("/api/user", (req, res) => {
-  const result = database.filter(
-    (user) => user.emailaddress == req.body.emailaddress
-  );
-  if (result.length > 0) {
-    res.status(409).json({
-      message:
-        "User was not added to database, because the email adress is already in use",
-    });
-  } else {
-    let user = req.body;
-    id++;
-    user = {
-      id,
-      firstName: user.firstName,
-      lastName: user.lastName,
-      street: user.street,
-      city: user.city,
-      emailAdress: user.emailAdress,
-      phonenNumber: user.phoneNumber,
-      password: user.password,
-      roles: user.roles,
-    };
-    console.log(user);
-    database.push(user);
-    res.status(201).json({
-      status: 201,
-      result: database,
-    });
-  }
-});
+router.post("/api/user", userController.addUser);
 
-router.get("/api/user", (req, res, next) => {
-  res.status(200).json({
-    status: 200,
-    result: database,
-  });
-});
+router.get("/api/user", userController.getAllUsers);
 
-router.get("/api/user/:userId", (req, res, next) => {
-  const userId = req.params.userId;
-  console.log(`User met ID ${userId} gezocht`);
-  let user = database.filter((item) => item.id == userId);
-  if (user.length > 0) {
-    console.log(user);
-    res.status(200).json({
-      status: 200,
-      result: user,
-    });
-  } else {
-    res.status(401).json({
-      status: 401,
-      result: `User with ID ${userId} not found`,
-    });
-  }
-});
+router.get("/api/user/:userId", userController.getUserById);
 
-router.get("/api/user/profile/:userId", (req, res, next) => {
-  const userId = req.params.userId;
-  console.log(`User met ID ${userId} gezocht`);
-  let user = database.filter((item) => item.id == userId);
-  if (user.length > 0) {
-    console.log(user);
-    res.status(401).json({
-      status: 401,
-      result: `Feature not realised yet`,
-    });
-  } else {
-    res.status(401).json({
-      status: 401,
-      result: `User with ID ${userId} not found, and this feature is not yet realised`,
-    });
-  }
-});
+router.get("/api/user/profile/:userId", userController.getUserProfile);
 
-router.put("/api/user/:userId", (req, res, next) => {
-  const userId = req.params.userId;
-  console.log(`User met ID ${userId} gezocht`);
-  const result = database.findIndex((user) => user.id == userId);
-  if (result > -1) {
-    let user = req.body;
-    database[result] = {
-      id: userId,
-      firstName: user.firstName,
-      lastName: user.lastName,
-      street: user.street,
-      city: user.city,
-      emailAdress: user.emailAdress,
-      phoneNumber: user.phoneNumber,
-      password: user.password,
-      roles: user.roles,
-    };
-    res.status(201).json({
-      status: 201,
-      result: database[result],
-    });
-  } else {
-    res.status(401).json({
-      status: 401,
-      result: `User with ID ${userId} not found`,
-    });
-  }
-});
+router.put("/api/user/:userId", userController.updateUser);
 
-router.delete("/api/user/:userId", (req, res, next) => {
-  const userId = req.params.userId;
-  console.log(`User met ID ${userId} gezocht`);
-  let user = database.filter((item) => item.id == userId);
-  if (user.length > 0) {
-    console.log(user);
-    database.splice(userId - 1, 1);
-    res.status(200).json({
-      status: 200,
-      result: `User with ID ${userId} succesfully deleted`,
-    });
-  } else {
-    res.status(401).json({
-      status: 401,
-      result: `User with ID ${userId} not found`,
-    });
-  }
-});
+router.delete("/api/user/:userId", userController.deleteUser);
 
 module.exports = router;
