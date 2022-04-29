334c40837c8016fb61894ea3863650cdcfcb1121
tnquartel <thomasquartel@outlook.com>
2022-04-29 09:12:00 +0200

    added tc 201-1 - 201-4

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -6,11 +6,18 @@ let controller = {
   validateUser: (req, res, next) => {
     let user = req.body;
     let { firstName, lastName, emailAdress, password } = user;
+    const result = database.filter(
+      (user) => user.emailaddress == req.body.emailaddress
+    );
     try {
       assert(typeof firstName === "string", "First name must be a string");
       assert(typeof lastName === "string", "Last name must be a string");
       assert(typeof emailAdress === "string", "Email address must be a string");
       assert(typeof password === "string", "Password must be a string");
+      assert(
+        result.length == 0,
+        "User was not added to database, because the email address is already in use"
+      );
       next();
     } catch (err) {
       const error = {
@@ -21,35 +28,25 @@ let controller = {
     }
   },
   addUser: (req, res) => {
-    const result = database.filter(
-      (user) => user.emailaddress == req.body.emailaddress
-    );
-    if (result.length > 0) {
-      res.status(409).json({
-        message:
-          "User was not added to database, because the email adress is already in use",
-      });
-    } else {
-      let user = req.body;
-      id++;
-      user = {
-        id,
-        firstName: user.firstName,
-        lastName: user.lastName,
-        street: user.street,
-        city: user.city,
-        emailAdress: user.emailAdress,
-        phoneNumber: user.phoneNumber,
-        password: user.password,
-        roles: user.roles,
-      };
-      console.log(user);
-      database.push(user);
-      res.status(201).json({
-        status: 201,
-        result: database,
-      });
-    }
+    let user = req.body;
+    id++;
+    user = {
+      id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      street: user.street,
+      city: user.city,
+      emailAdress: user.emailAdress,
+      phoneNumber: user.phoneNumber,
+      password: user.password,
+      roles: user.roles,
+    };
+    console.log(user);
+    database.push(user);
+    res.status(200).json({
+      status: 200,
+      result: user,
+    });
   },
   getAllUsers: (req, res) => {
     res.status(200).json({
--- test/intergration/user.controller.test.js
+++ test/intergration/user.controller.test.js
@@ -1,13 +1,15 @@
 const chai = require("chai");
 const chaiHttp = require("chai-http");
+const { post } = require("../../index");
 const server = require("../../index");
+const { addUser } = require("../../src/controllers/user.controller");
 let database = [];
 
 chai.should();
 chai.use(chaiHttp);
 
 describe("Manage users", () => {
-  describe("UC-201 add user /api/user", () => {
+  describe("TC-201-1 add user /api/user", () => {
     beforeEach((done) => {
       database = [];
       done();
@@ -17,9 +19,35 @@ describe("Manage users", () => {
         .request(server)
         .post("/api/user")
         .send({
+          //first name ontbreekt,
+          lastName: "Quartel",
+          emailAdress: "thomasquartel@outlook.com",
+          password: "password",
+        })
+        .end((err, res) => {
+          res.should.be.an("object");
+          let { status, result } = res.body;
+          status.should.equals(400);
+          result.should.be
+            .a("string")
+            .that.equals("First name must be a string");
+          done();
+        });
+    });
+  });
+  describe("TC-201-2 add user /api/user", () => {
+    beforeEach((done) => {
+      database = [];
+      done();
+    });
+    it("When a invalid email address is entered, a valid error should be returned", (done) => {
+      chai
+        .request(server)
+        .post("/api/user")
+        .send({
           firstName: "Thomas",
           lastName: "Quartel",
-          //email addres ontbreekt
+          emailAdress: 100,
           password: "password",
         })
         .end((err, res) => {
@@ -29,6 +57,90 @@ describe("Manage users", () => {
           result.should.be
             .a("string")
             .that.equals("Email address must be a string");
+          done();
+        });
+    });
+  });
+  describe("TC-201-3 add user /api/user", () => {
+    beforeEach((done) => {
+      database = [];
+      done();
+    });
+    it("When a invalid password is entered, a valid error should be returned", (done) => {
+      chai
+        .request(server)
+        .post("/api/user")
+        .send({
+          firstName: "Thomas",
+          lastName: "Quartel",
+          emailAdress: "thomasquartel@outlook.com",
+          password: 100,
+        })
+        .end((err, res) => {
+          res.should.be.an("object");
+          let { status, result } = res.body;
+          status.should.equals(400);
+          result.should.be.a("string").that.equals("Password must be a string");
+          done();
+        });
+    });
+  });
+  describe("TC-201-4 add user /api/user", () => {
+    beforeEach((done) => {
+      database = [];
+      done();
+    });
+    it("When a user that already exists is added, a valid error should be returned", (done) => {
+      const user = {
+        firstName: "Thomas",
+        lastName: "Quartel",
+        emailAdress: "thomasquartel@outlook.com",
+        password: "password",
+      };
+      chai.request(server).post("/api/user").send(user).end();
+      chai
+        .request(server)
+        .post("/api/user")
+        .send({
+          firstName: "Thomas",
+          lastName: "Quartel",
+          emailAdress: "thomasquartel@outlook.com",
+          password: "password",
+        })
+        .end((err, res) => {
+          res.should.be.an("object");
+          let { status, result } = res.body;
+          status.should.equals(400);
+          result.should.be
+            .a("string")
+            .that.equals(
+              "User was not added to database, because the email address is already in use"
+            );
+          done();
+        });
+    });
+  });
+  describe("TC-201-5 add user /api/user", () => {
+    beforeEach((done) => {
+      database = [];
+      done();
+    });
+    it("When a new user is added, the user should be returend", (done) => {
+      const user = {
+        firstName: "Thomas",
+        lastName: "Quartel",
+        emailAdress: "testmail@mail.com",
+        password: "password",
+      };
+      chai
+        .request(server)
+        .post("/api/user")
+        .send(user)
+        .end((err, res) => {
+          res.should.be.an("object");
+          let { status, result } = res.body;
+          status.should.equals(200);
+          result.firstName.should.equals(user.firstName);
           done();
         });
     });
